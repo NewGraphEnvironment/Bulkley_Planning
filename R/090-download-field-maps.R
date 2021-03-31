012dd0a0f9e45ebab05dda2c110ee6cd3037e1a4
NewGraphEnvironment <al@newgraphenvironment.com>
2021-03-31 09:04:23 -0700

    download latest mapsets for archiving

--- R/090-download-field-maps.R
+++ R/090-download-field-maps.R
@@ -1,5 +1,6 @@
 require(RCurl)
 library(XML)
+library(magrittr)
 
 ##we are going to do this a bit sloppy and set a working directory
 setwd("C:/Users/allan/OneDrive/New_Graph/Current/2019-023_Bulkley_fish_passage/field_maps/moe_2018")
@@ -51,3 +52,28 @@ for (filename in filenames) {
                                                       sep = ""), mode = "wb")
 }
 
+##archive the maps used for Bulk planning 
+path = "C:/Users/allan/OneDrive/New_Graph/Current/2019-023_Bulkley_fish_passage/field_maps/2020_planning/BULK"
+url = "https://www.hillcrestgeo.ca/outgoing/fishpassage/projects/bulkley/archive/2020-09-01/"
+
+filenames <- getURL(url,verbose=TRUE,ftp.use.epsv=TRUE, dirlistonly = TRUE) %>% 
+  getHTMLLinks(xpQuery = "//a/@href[contains(., '.pdf')]") #https://stackoverflow.com/questions/32213591/list-files-on-http-ftp-server-in-r  
+
+##this could prb just be an lapply but not going to look at it now. just recycle from last project and go to bed.
+for (filename in filenames) {
+  download.file(paste(url, filename, sep = ""), paste(path, "/", filename,
+                                                      sep = ""), mode = "wb")
+}
+
+##archive the maps used for MORR planning 
+path = "C:/Users/allan/OneDrive/New_Graph/Current/2019-023_Bulkley_fish_passage/field_maps/2020_planning/MORR"
+url = "https://www.hillcrestgeo.ca/outgoing/fishpassage/projects/morice/"
+
+filenames <- getURL(url,verbose=TRUE,ftp.use.epsv=TRUE, dirlistonly = TRUE) %>% 
+  getHTMLLinks(xpQuery = "//a/@href[contains(., '.pdf')]") #https://stackoverflow.com/questions/32213591/list-files-on-http-ftp-server-in-r  
+
+##this could prb just be an lapply but not going to look at it now. just recycle from last project and go to bed.
+for (filename in filenames) {
+  download.file(paste(url, filename, sep = ""), paste(path, "/", filename,
+                                                      sep = ""), mode = "wb")
+}
